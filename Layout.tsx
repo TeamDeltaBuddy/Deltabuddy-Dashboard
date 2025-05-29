6bff23e28b2ef79e2ed5b0dd73059ae52fa6776c
TeamDeltaBuddy <mirza.hassanuzzaman@gmail.com>
2025-05-29 16:01:43 +0530

    Update Layout.tsx

--- Layout.tsx
+++ Layout.tsx
@@ -1,15 +1,15 @@
 import React, { useState, ReactNode } from 'react';
 
-type LayoutProps = {
+interface LayoutProps {
   children: ReactNode;
-};
+}
 
 export default function Layout({ children }: LayoutProps) {
   const [open, setOpen] = useState(false);
 
   return (
     <div>
-      {/* Your layout logic here */}
+      {/* Your layout JSX here */}
       {children}
     </div>
   );
